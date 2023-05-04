1623aff95345821dc0d754e512d5f49324e2bc10
Luan Albuquerque <luan.santos6605@gmail.com>
2023-05-04 19:46:51 -0400

    admin

--- src/shared/http/middlewares/admin.middleware.ts
+++ src/shared/http/middlewares/admin.middleware.ts
@@ -3,7 +3,7 @@ import { UnauthorizedException } from "@nestjs/common";
 
 function EnsureAdminMiddleware(request: Request, response: Response, next: NextFunction) {
     const user = request.user
-    if (user.is_admin) {
+    if (!user.is_admin) {
         throw new UnauthorizedException('User Unauthorized')
     }
     next()
