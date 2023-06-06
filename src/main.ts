0d2c238068f2e68321b7d7505620882d29002816
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2023-06-06 16:05:25 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(3000);
+  await app.listen(3100);
 }
 bootstrap();
