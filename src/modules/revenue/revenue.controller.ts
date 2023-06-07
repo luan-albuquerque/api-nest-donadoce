1a2c84de339683881cc1a5be1dbd1d706cf406cd
Luan Albuquerque <luan.santos6605@gmail.com>
2023-06-07 08:10:27 -0400

    delete

--- src/modules/revenue/revenue.controller.ts
+++ src/modules/revenue/revenue.controller.ts
@@ -114,7 +114,7 @@ export class RevenueController {
     await this.updateRevenueService.execute(id, newData)
   }
 
-
+  @Delete()
   @ApiOperation({ summary: "EndPoint de deletar receita com todos os ingredientes compostos nela.", description: ""})
   remove(@Param('fk_revenue') fk_revenue: string) {
     return this.deleteRevenueService.execute(fk_revenue);
