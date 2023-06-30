0557b14e4c59287250754695cf1140d10f755005
Luan Albuquerque <luan.santos6605@gmail.com>
2023-06-29 21:47:23 -0400

    Person

--- src/modules/company/services/update-company.service.ts
+++ src/modules/company/services/update-company.service.ts
@@ -23,24 +23,39 @@ export class UpdateCompanyService {
       throw new HttpException('id não existe', HttpStatus.CONFLICT)
     }
 
-    const findByEmailInUser = await this.userRepository.findByMail(updateCompanyDto.email);
     const findByEmail = await this.companyRepository.findByEmail(updateCompanyDto.email);
-    if (findByEmail && updateCompanyDto.email != findByEmail.email || findByEmailInUser) {
+
+    if (findByEmail && updateCompanyDto.email != findByEmail.email) {
       throw new NotFoundException("Email já Existe")
     }
 
-    const findByCNPJInClient = await this.clientsRepository.findByCNPJ(updateCompanyDto.cnpj);
+    const findByEmailInUser = await this.userRepository.findByMail(updateCompanyDto.email);
+
+    if (findByEmailInUser) {
+      throw new NotFoundException("Email utilizado por um usuario")
+    }
+
     const findByCNPJ = await this.companyRepository.findByCNPJ(updateCompanyDto.cnpj);
-    if (findByCNPJ  && updateCompanyDto.cnpj != findByCNPJ.cnpj || findByCNPJInClient) {
+    if (findByCNPJ && updateCompanyDto.cnpj != findByCNPJ.cnpj ) {
       throw new NotFoundException("CNPJ já Existe.")
     }
 
+    const findByCNPJInClient = await this.clientsRepository.findByCNPJ(updateCompanyDto.cnpj);
+
+    if (findByCNPJInClient ) {
+      throw new NotFoundException("CNPJ já utilizado em um clente.")
+    }
+
     const findByFone = await this.companyRepository.findByFone(updateCompanyDto.fone);
-    const findByFoneInClient = await this.clientsRepository.findByFone(updateCompanyDto.fone);
-    if (findByFone  && updateCompanyDto.fone != findByFone.fone || findByFoneInClient) {
+    if (findByFone  && updateCompanyDto.fone != findByFone.fone) {
       throw new NotFoundException("Fone já Existe.")
     }
 
+    const findByFoneInClient = await this.clientsRepository.findByFone(updateCompanyDto.fone);
+
+    if (findByFoneInClient) {
+      throw new NotFoundException("Fone utilizado por um cliente.")
+    }
   
     const create: UpdateCompanyDto = {
       corporate_name: updateCompanyDto.corporate_name,
