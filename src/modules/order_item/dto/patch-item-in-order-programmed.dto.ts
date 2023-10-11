230041127677f3e3e2f7bd625ec340935ee876e6
luan santos <luan.santos@grupoicts.com.br>
2023-10-11 17:07:27 -0400

    OrderItem

--- src/modules/order_item/dto/patch-item-in-order-programmed.dto.ts
+++ src/modules/order_item/dto/patch-item-in-order-programmed.dto.ts
@@ -1,8 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
+
 export class PatchItemInOrderDTO {
 
+    @IsString({ message: 'Variável fk_order precisa ser string' })
+    @IsNotEmpty({ message: 'Variável fk_order não pode ser vazia' })
+    @ApiProperty()
     fk_order: string
+
+    @IsString({ message: 'Variável fk_revenue precisa ser string' })
+    @IsNotEmpty({ message: 'Variável fk_revenue não pode ser vazia' })
+    @ApiProperty()
     fk_revenue: string
+
+    @IsString({ message: 'Variável fk_categoryOrderItem precisa ser string' })
+    @IsNotEmpty({ message: 'Variável fk_categoryOrderItem não pode ser vazia' })
+    @ApiProperty()
     fk_categoryOrderItem: string
+
+    @IsNumber()
+    @ApiProperty()
     amountItem: number
 
 }
--- src/modules/order_item/dto/remove-item-in-order-programmed.dto.ts
+++ src/modules/order_item/dto/remove-item-in-order-programmed.dto.ts
@@ -1,7 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsString } from "class-validator"
+
 export class RemoveItemInOrderDTO {
 
+    @IsString({ message: 'Variável fk_order precisa ser string' })
+    @IsNotEmpty({ message: 'Variável fk_order não pode ser vazia' })
+    @ApiProperty()
     fk_order: string
+
+    @IsString({ message: 'Variável fk_revenue precisa ser string' })
+    @IsNotEmpty({ message: 'Variável fk_revenue não pode ser vazia' })
+    @ApiProperty()
     fk_revenue: string
+
+    @IsString({ message: 'Variável fk_categoryOrderItem precisa ser string' })
+    @IsNotEmpty({ message: 'Variável fk_categoryOrderItem não pode ser vazia' })
+    @ApiProperty()
     fk_categoryOrderItem: string
 
 }
