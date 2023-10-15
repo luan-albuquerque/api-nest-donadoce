2390c28b7110c341249ee6b7a0efa10f4f96c67c
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2023-10-15 18:21:06 -0400

    Atualizar o patch-status-order.dto.ts

--- src/modules/clients/dto/patch-status-order.dto.ts
+++ src/modules/clients/dto/patch-status-order.dto.ts
@@ -6,7 +6,7 @@ export class PatchStatusOrderDTO {
     @IsString({ message: 'Esta variável de fk_order_Status precisa ser string' })
     @IsNotEmpty({ message: 'Esta variável de fk_order_Status não pode esvaziar' })
     @ApiProperty()
-    fk_order_Status: string
+    fk_order_status: string
 
     @IsString({ message: 'Esta variável de comment precisa ser string' })
     @ApiProperty()
