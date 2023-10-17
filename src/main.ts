60cc88ecba7955a8ba008ee2424c367266e4d534
luan santos <luan.santos@grupoicts.com.br>
2023-10-17 16:15:35 -0400

    ajuste de horarios no backend

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './shared/config/swagger/swagger-config';
 import * as dayjs from "dayjs"
+import * as utc from "dayjs/plugin/utc"
+import * as timezone  from "dayjs/plugin/timezone"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.tz.setDefault('America/Manaus');
   
-  console.log({
-    gte: dayjs().minute(0).second(0).millisecond(0).toDate(),
-    lte: dayjs().minute(0).second(0).millisecond(0).add(1, 'day').toDate(),
-    t: dayjs().toDate(),
-  });
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
