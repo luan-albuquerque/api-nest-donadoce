0dcca4fc7bc03fe5e9b7065c67a9b3bb2d7a3af7
luan santos <luan.santos@grupoicts.com.br>
2023-10-26 17:03:12 -0400

    ajuste de data

--- src/modules/control_production/dtos/patch-set-control-production.dto.ts
+++ src/modules/control_production/dtos/patch-set-control-production.dto.ts
@@ -1,7 +1,13 @@
+import { ApiOperation, ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty } from "class-validator"
 import { OrderType } from "src/modules/order/types/ordertype.type"
 
 export class PatchSetControlProductionProductDto {
-  
+    @IsNotEmpty({ message: 'Campo de id não pode ser vazio' })
+    @ApiProperty()
     id: string
+
+    @IsNotEmpty({ message: 'Campo de set não pode ser vazio' })
+    @ApiProperty()
     set: number
 }
