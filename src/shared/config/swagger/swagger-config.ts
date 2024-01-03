de78a0081e1be9cccc49f7b3c37e33427317dc17
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2024-01-03 10:25:37 -0400

    Atualizar o swagger-config.ts

--- src/shared/config/swagger/swagger-config.ts
+++ src/shared/config/swagger/swagger-config.ts
@@ -5,7 +5,7 @@ const swaggerConfig = new DocumentBuilder()
   .setDescription('')
   .setContact('Dona Doce', '', '')
   .setVersion('1.0')
-  .addServer(`https://api.donadoce.net)
+  .addServer("https://api.donadoce.net")
   .addServer(`http://localhost:${3100}`)
   .addBearerAuth()
   .build();
