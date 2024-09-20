1b762ad35e46b8293ccd0399a5ac2de195c75dfc
LUAN ALBUQUERQUE DOS SANTOS <luan.santos@indt.org.br>
2024-09-20 16:42:04 -0400

    fix/patch-order-status

--- src/modules/control_production/dtos/find-item-production.dto.ts
+++ src/modules/control_production/dtos/find-item-production.dto.ts
@@ -1,8 +1,8 @@
 
 export class FindItemProductionDto {
 
-    fk_revenue: string
-    fk_categoryOrderItem: string
-    delivery_date: Date
+    fk_revenue?: string
+    fk_categoryOrderItem?: string
+    delivery_date?: Date
 
 }
